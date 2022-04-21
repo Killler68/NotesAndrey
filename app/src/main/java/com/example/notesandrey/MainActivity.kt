7f77805d76827aa5cc0a5b57e63795a444655a60
VIP <belevitin2010@mail.ru>
2022-04-21 17:27:28 +0300

    feature/create-fragments-for-tasks

--- app/src/main/java/com/example/notesandrey/MainActivity.kt
+++ app/src/main/java/com/example/notesandrey/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.notesandrey
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.notesandrey.extensions.navigateToFragment
 
-class MainActivity : Activity() {
+
+class MainActivity : AppCompatActivity() {
+    private val button1: Button by lazy {
+        findViewById(R.id.btn_fragment1)
+    }
+    private val button2: Button by lazy {
+        findViewById(R.id.btn_fragment2)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        button1.setOnClickListener {
+            navigateToFragment(FirstFragment())
+        }
+        button2.setOnClickListener {
+            navigateToFragment(SecondFragment())
+        }
+
     }
 }
