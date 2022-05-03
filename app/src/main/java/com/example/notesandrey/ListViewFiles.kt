079b759a2ad12726927b536d64c33cdd3cbd55e0
VIP <belevitin2010@mail.ru>
2022-05-04 01:27:36 +0300

    Fix list main

---
+++ app/src/main/java/com/example/notesandrey/ListViewFiles.kt
@@ -0,0 +1,29 @@
+package com.example.notesandrey
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+
+class ListViewFiles : Fragment() {
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_view_files, container, false)
+    }
+}
--- app/src/main/java/com/example/notesandrey/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/example/notesandrey/extensions/FragmentExtensions.kt
@@ -5,7 +5,7 @@ import com.example.notesandrey.R
 
 fun Fragment.navigateToFragment(fragment: Fragment) {
     this.parentFragmentManager.beginTransaction()
-        .replace(R.id.fon2, fragment, fragment.javaClass.simpleName)
+        .replace(R.id.background_main, fragment, fragment.javaClass.simpleName)
         .addToBackStack(fragment.javaClass.simpleName)
         .commit()
 }
