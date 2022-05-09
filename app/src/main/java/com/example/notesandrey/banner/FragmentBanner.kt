93c668a9f3590a39dc10299d153410a4df618b4b
VIP <belevitin2010@mail.ru>
2022-05-09 22:31:54 +0300

    Added sounds to the banner and changed ImageView to ImageButton

--- app/src/main/java/com/example/notesandrey/banner/FragmentBanner.kt
+++ app/src/main/java/com/example/notesandrey/banner/FragmentBanner.kt
@@ -1,14 +1,20 @@
 package com.example.notesandrey.banner
 
+import android.media.MediaPlayer
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.notesandrey.R
+import com.example.notesandrey.databinding.FragmentBannerBinding
 
 
 class FragmentBanner : Fragment() {
+
+    private var _binding: FragmentBannerBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +26,18 @@ class FragmentBanner : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentBannerBinding.bind(view)
+        soundMeow()
+    }
+
+    private fun soundMeow() {
+        binding.banner.setOnClickListener {
+            mediaPlayer = MediaPlayer.create(activity, R.raw.catmeow1)
+            mediaPlayer.start()
+        }
+        binding.poster.setOnClickListener {
+            mediaPlayer = MediaPlayer.create(activity, R.raw.catmeow2)
+            mediaPlayer.start()
+        }
     }
 }
